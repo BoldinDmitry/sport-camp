a34c53f5934f18df26d8d474383df62d32b62ef1
Katherin_Z <zyrianovakate10@gmail.com>
2020-08-05 11:47:29 +0300

    wip

---
+++ main.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', ()=>{
+
+
+let coach_1 = document.getElementById('#coach_1')
+let lad = document.getElementById('#hidden_1')
+if(coach_1){
+coach_1.addEventListener('mouseenter', function(){
+ hidden_1.style.display = 'block'
+})
+}
+
+// })
