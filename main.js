49d4d3a153077e42acda8314e3fdb098e73f52c8
Katherin_Z <zyrianovakate10@gmail.com>
2020-08-07 11:35:14 +0300

    wip

--- main.js
+++ main.js
@@ -5,8 +5,33 @@ let coach_1 = document.getElementById('#coach_1')
 let lad = document.getElementById('#hidden_1')
 if(coach_1){
 coach_1.addEventListener('mouseenter', function(){
- hidden_1.style.display = 'block'
+ hidden_1.style.marginTop = '9vw'
 })
+
+coach_1.onclick = function(){
+  lad.style.opacity = '1'
 }
 
+let windw = this
+
+$.fn.followTo = function(pos){
+  let $this = this,
+  $window = $(windw)
+
+  $window.scroll(function(e){
+   if($window.scrollTop() > pos){
+     $this.css({
+       position: 'absolute',
+       top: pos
+     })
+   } else{
+     $this.css({
+       position: 'fixed',
+       top: 0
+     })
+   }
+
+  })
+}
+ $('#f').followTo(250)
 // })
