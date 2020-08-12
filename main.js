1ce651d91d0f8874d71c83741f901c0d5fa73be5
Katherin_Z <zyrianovakate10@gmail.com>
2020-08-12 16:18:15 +0300

    main mobile done

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
-// document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
 
 
-let coach_1 = document.getElementById('#coach_1')
-let lad = document.getElementById('#hidden_1')
-if(coach_1){
-coach_1.addEventListener('mouseenter', function(){
- hidden_1.style.marginTop = '9vw'
+
+
+document.getElementsByClassName('.name').onmouseover = function(){
+  document.querySelector('.name img').style.zIndex = '100'
+}
+
+// let windw = this
+//
+// $.fn.followTo = function(pos){
+//   let $this = this,
+//   $window = $(windw)
+//
+//   $window.scroll(function(e){
+//    if($window.scrollTop() > pos){
+//      $this.css({
+//        position: 'absolute',
+//        top: pos
+//      })
+//    } else{
+//      $this.css({
+//        position: 'fixed',
+//        top: 0
+//      })
+//    }
+//
+//   })
+// }
+//  $('#f').followTo(250)
 })
-
-coach_1.onclick = function(){
-  lad.style.opacity = '1'
-}
-
-let windw = this
-
-$.fn.followTo = function(pos){
-  let $this = this,
-  $window = $(windw)
-
-  $window.scroll(function(e){
-   if($window.scrollTop() > pos){
-     $this.css({
-       position: 'absolute',
-       top: pos
-     })
-   } else{
-     $this.css({
-       position: 'fixed',
-       top: 0
-     })
-   }
-
-  })
-}
- $('#f').followTo(250)
-// })
