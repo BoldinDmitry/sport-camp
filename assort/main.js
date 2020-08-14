ff2554d3417cec408bc5683c31f62d229e988419
Katherin_Z <zyrianovakate10@gmail.com>
2020-08-14 23:57:10 +0300

    wip

--- assort/main.js
+++ assort/main.js
@@ -3,11 +3,11 @@ let live = document.querySelector('#accom')
 let bikes = document.querySelector('#assort')
 let serve = document.querySelector('#serve')
 live.onmouseover = function(){
-  line.style.left = '45vw'
+  line.style.left = '30vw'
   line.style.width = '9vw'
 }
 live.onmouseout = function(){
-  line.style.left = '64vw'
+  line.style.left = '58vw'
   line.style.width = '10.5vw'
 }
 
@@ -20,9 +20,9 @@ serve.onmouseout = function(){
   line.style.width = '10.5vw'
 }
 
-let btn = document.querySelectorAll('#btn')
-let open = document.querySelector('#form iframe')
-
-btn.onclick = function(){
-  open.style.display = 'block'
-}
+// let btn = document.querySelectorAll('#btn')
+// let open = document.querySelector('#form iframe')
+//
+// btn.onclick = function(){
+//   open.style.display = 'block'
+// }
